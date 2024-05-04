e2d7492dcf02686455340d308996ed53699fa236
jj-jh <junhee4942@naver.com>
2024-05-05 00:27:56 +0900

    change main.dart

--- Study_Flutter/flutter_application_1/lib/main.dart
+++ Study_Flutter/flutter_application_1/lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const App());
+  runApp(App());
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color(0xFF181818),
+        backgroundColor: Color(0xFF181818),
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
+          padding: EdgeInsets.symmetric(horizontal: 40),
           child: Column(
             children: [
-              const SizedBox(
+              SizedBox(
                 height: 50,
               ),
               Row(
@@ -25,7 +23,7 @@ class App extends StatelessWidget {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      const Text(
+                      Text(
                         'Hey, Selena',
                         style: TextStyle(
                           color: Colors.white,
