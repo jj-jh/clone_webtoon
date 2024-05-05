325953dd2e304b222c03c9b9245ee5d1e5569eea
jj-jh <junhee4942@naver.com>
2024-05-05 12:08:25 +0900

    change color

--- Study_Flutter/flutter_application_1/lib/main.dart
+++ Study_Flutter/flutter_application_1/lib/main.dart
@@ -34,7 +34,7 @@ class App extends StatelessWidget {
                       Text(
                         'welcome back',
                         style: TextStyle(
-                          color: Colors.red,
+                          color: Colors.white.withOpacity(0.7),
                           fontSize: 18,
                         ),
                       ),
